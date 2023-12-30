cababdb9f19e9fc2aac3b379122134906bb65d1c
ellieburger <lee.liz2002ilovenh1d520@gmail.com>
2023-12-30 19:52:59 +0800

    pj

---
+++ app/src/main/java/com/example/happy_jellyfish/flappybird/FlappyBirdView.kt
@@ -0,0 +1,4 @@
+package com.example.happy_jellyfish
+
+class FlappyBirdView {
+}
---
+++ app/src/main/java/com/example/happy_jellyfish/flappybird/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.happy_jellyfish.flappybird
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.happy_jellyfish.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_flappybird)
+    }
+}
